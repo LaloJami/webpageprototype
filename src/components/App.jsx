484d022fb6a7d0aab7c7e8401512b68648fc7129
LaloJami <eduardolalojami@gmail.com>
2021-10-08 22:07:56 -0500

    Configuracion basica react

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/global.scss'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola Mundo! Este es el curso practico de React</h1>
+    </div>
+  );
+};
+
+export default App;
